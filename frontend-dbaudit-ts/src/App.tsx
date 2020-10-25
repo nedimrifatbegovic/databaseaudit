c452eace8b5074ab0aa16ab6bf059baeaabc5971
unknown <beclija@outlook.de>
2020-10-25 14:18:17 -0700

    #12 simple router added

--- frontend-dbaudit-ts/src/App.tsx
+++ frontend-dbaudit-ts/src/App.tsx
@@ -1,7 +1,40 @@
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import { Container } from "react-bootstrap";
 import React from "react";
+import styled from "styled-components";
+
+// * Styled Components
+const AppWrapper = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`;
+
+const MainSection = styled.div`
+  flex-grow: 1;
+`;
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <AppWrapper>
+      <Router>
+        <MainSection>
+          <Switch>
+            <Route exact path="/">
+              <div>Home Page</div>
+            </Route>
+            <Route exact path="/login">
+              <div>Login Page</div>
+            </Route>
+            <Route>
+              <div>Error 404</div>
+            </Route>
+          </Switch>
+        </MainSection>
+      </Router>
+    </AppWrapper>
+  );
 }
 
 export default App;
