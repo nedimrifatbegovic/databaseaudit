a7a2b516d967e3186915c4c2f184ba86ec1b3cc5
unknown <beclija@outlook.de>
2020-10-26 14:41:15 -0700

    #12 frontend for remove user added

---
+++ frontend-dbaudit-ts/src/components/Admin/components/RemoveUser/RemoveUser.resources.ts
@@ -0,0 +1,10 @@
+export const description = {
+  title: "Remove existing user",
+  email: "Email: ",
+  emailmsg: "Please enter the users email",
+  type: "Type:",
+  internalValue: "internal",
+  internalText: "Internal Auditor",
+  externalValue: "external",
+  externalText: "External Auditor",
+};
