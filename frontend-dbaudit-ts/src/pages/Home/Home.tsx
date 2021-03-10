10ef813b196802e5d8a25f38752e86476ab15749
unknown <beclija@outlook.de>
2021-03-10 20:59:01 +0100

    home text added

--- frontend-dbaudit-ts/src/pages/Home/Home.tsx
+++ frontend-dbaudit-ts/src/pages/Home/Home.tsx
@@ -17,12 +17,45 @@ const Paragraph = styled.p`
   font-size: 1.2rem;
 `;
 
+const StyledLi = styled.li`
+  font-size: 1.2rem;
+`;
+
 export default function Home() {
   return (
     <React.Fragment>
       <Title>{resources.title}</Title>
       <Paragraph>{resources.subtitle}</Paragraph>
       <Paragraph>{resources.description}</Paragraph>
+      <ul>
+        <StyledLi>{resources.list1}</StyledLi>
+        <StyledLi>{resources.list2}</StyledLi>
+        <StyledLi>{resources.list3}</StyledLi>
+        <StyledLi>{resources.list4}</StyledLi>
+        <StyledLi>{resources.list5}</StyledLi>
+        <StyledLi>{resources.list6}</StyledLi>
+        <StyledLi>{resources.list7}</StyledLi>
+        <StyledLi>{resources.list8}</StyledLi>
+      </ul>
+      <hr />
+      <Paragraph>{resources.scenariotitle}</Paragraph>
+      <ul>
+        <StyledLi>{resources.admin}</StyledLi>
+        <StyledLi>{resources.internalauditor}</StyledLi>
+        <StyledLi>{resources.externalauditor}</StyledLi>
+      </ul>
+      <hr />
+      <Paragraph>{resources.architecture}</Paragraph>
+      <ul>
+        <StyledLi>{resources.backend}</StyledLi>
+        <StyledLi>{resources.frontend}</StyledLi>
+        <StyledLi>{resources.packages}</StyledLi>
+      </ul>
+      <hr />
+      <Paragraph>{resources.author}</Paragraph>
+      <Paragraph>{resources.mentor}</Paragraph>
+      <Paragraph>{resources.university}</Paragraph>
+      <Paragraph>{resources.documentation}</Paragraph>
     </React.Fragment>
   );
 }
