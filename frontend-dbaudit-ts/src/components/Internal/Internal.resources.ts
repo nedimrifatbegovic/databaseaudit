935f0c08695e1a4de89a4cf47b884a0b5743ca26
unknown <beclija@outlook.de>
2021-03-10 23:40:42 +0100

    text adjusted

--- frontend-dbaudit-ts/src/components/Internal/Internal.resources.ts
+++ frontend-dbaudit-ts/src/components/Internal/Internal.resources.ts
@@ -1,9 +1,9 @@
 export const description = {
-  title: "Welcome to the Audit Home Page",
+  title: "Welcome to the Internal Audit Home Page",
   overviewTitle:
     "Overview of External Audit Requests (Pending / Accepted / Declined)",
   configTitle: "Set the Audit Configuration",
   auditTitle: "Audit your Database",
   previousAuditsTitle: "Overview of Previous Audits",
-  configRequestTitle: "Pending Requests for needed Configuration",
+  configRequestTitle: "Pending Requests for Needed Configuration",
 };
